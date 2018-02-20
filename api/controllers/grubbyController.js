1e9fce75571f47f8d3b79b950616cc220c225788
Caitlin <ayashi@valefor.com>
2018-02-19 22:30:46 -0800

    404 when image not found, resize images to one static size (need to take in sizes from url next)

--- api/controllers/grubbyController.js
+++ api/controllers/grubbyController.js
@@ -2,23 +2,29 @@
 const grubbys = require('../models/grubbyModel');
 const url = require('url');
 const path = require('path');
+const Jimp = require('jimp');
 
 function sendGrubby(file, res) {
   res.sendFile(file, { root: path.join(__dirname, '..', '..', 'images')});
 }
 
-exports.getSpecificGrubby = function(req, res) {
+exports.getSpecificGrubby = function(req, res, next) {
   const id = req.params.id;
   if(grubbys[id]) {
-    sendGrubby(grubbys[id], res);
-  } else {
-    //err
+    return Jimp.read(path.join(__dirname, '..', '..', 'images', grubbys[id]))
+      .then( img => img.clone())
+      .then( img => img.resize(700,500))
+      .then( img => img.getBuffer(Jimp.MIME_JPEG, function(err, buffer){
+        res.set('Content-Type', 'image/jpeg');
+        res.send(buffer);
+      }));
   }
+  //next('image not found');
+  next();
 }
 
 exports.getRandomGrubby = function(req,res) {
   const randomNumber = Math.floor(Math.random() * (grubbys.length));
   console.log(grubbys[randomNumber]);
   sendGrubby(grubbys[randomNumber], res);
-  // later: use xsendfile with nginx unless we do cropping resizing etc then that doesn't make sense
 }
--- api/routes/grubbyRoutes.js
+++ api/routes/grubbyRoutes.js
@@ -1,11 +1,8 @@
 module.exports = function(app) {
   const grubby = require('../controllers/grubbyController');
 
-  app.route('/random')
-    .get(grubby.getRandomGrubby);
-
-  app.route('/grubby/:id')
-    .get(grubby.getSpecificGrubby);
-
-  //app.route('/random-image')
+  app.get('/random', grubby.getRandomGrubby);
+  app.get('/grubby/:id', grubby.getSpecificGrubby);
+  app.use(function(req, res){ res.status(404).json({ '404': 'not found' })})
+  //app.use(function(err, req, res, next){ res.status(500).json({ '500': err })})
 }
