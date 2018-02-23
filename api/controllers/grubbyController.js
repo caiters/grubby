be232fe84e9e1195dca6bd2679a42fb3abb36575
Caitlin <ayashi@valefor.com>
2018-02-22 20:49:07 -0800

    serve up random images and regular images with the same route now

--- api/controllers/grubbyController.js
+++ api/controllers/grubbyController.js
@@ -4,30 +4,21 @@ const url = require('url');
 const path = require('path');
 const Jimp = require('jimp');
 
-function sendGrubby(file, res) {
-  res.sendFile(file, { root: path.join(__dirname, '..', '..', 'images')});
-}
-
-exports.getSpecificGrubby = function(req, res, next) {
+exports.getGrubby = function(req, res, next) {
   const id = req.params.id;
+  const grubbyId = id === 'random' ? Math.floor(Math.random() * (grubbys.length)) : id;
   const dimensions = req.params.dimensions.split('x');
   const width = Number(dimensions[0]);
   const height = Number(dimensions[1]);
-  if(grubbys[id]) {
-    return Jimp.read(path.join(__dirname, '..', '..', 'images', grubbys[id]))
-      .then( img => img.clone())
-      .then( img => img.cover(width,height))
-      .then( img => img.getBuffer(Jimp.MIME_JPEG, function(err, buffer){
-        res.set('Content-Type', 'image/jpeg');
-        res.send(buffer);
-      }));
-  }
-  //next('image not found');
-  next();
-}
 
-exports.getRandomGrubby = function(req,res) {
-  const randomNumber = Math.floor(Math.random() * (grubbys.length));
-  console.log(grubbys[randomNumber]);
-  sendGrubby(grubbys[randomNumber], res);
+  if (grubbys[grubbyId]) {
+    return Jimp.read(path.join(__dirname, '..', '..', 'images', grubbys[grubbyId]))
+    .then( img => img.clone())
+    .then( img => img.cover(width, height))
+    .then( img => img.getBuffer(Jimp.MIME_JPEG, function(err, buffer){
+      res.set('Content-Type', 'image/jpeg');
+      res.send(buffer);
+    }));
+    next();
+  }
 }
--- api/routes/grubbyRoutes.js
+++ api/routes/grubbyRoutes.js
@@ -1,8 +1,11 @@
+const express = require('express');
+const path = require('path');
+
 module.exports = function(app) {
   const grubby = require('../controllers/grubbyController');
 
-  app.get('/random', grubby.getRandomGrubby);
-  app.get('/grubby/:id/:dimensions', grubby.getSpecificGrubby);
+  app.get('/', express.static('./public'));
+  app.get('/grubby/:id/:dimensions', grubby.getGrubby);
   app.use(function(req, res){ res.status(404).json({ '404': 'not found' })})
   //app.use(function(err, req, res, next){ res.status(500).json({ '500': err })})
 }
